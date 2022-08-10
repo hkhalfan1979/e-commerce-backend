6fabf4bf8a93d2f546eef83428a7b406213b8d78
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-08-10 19:28:46 -0400

    added/updated models and updated sequalize

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // * Validates that the value is a decimal.
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // * Set a default value of `10`.
+      defaultValue: 10,
+      // * Validates that the value is numeric.
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // * References the `Category` model's `id`.
+      references: {
+      model: 'category',
+      key: 'id'
+      }
+    },
+    
   },
   {
     sequelize,
