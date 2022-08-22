2150f335f3b7c5d337a1926570790b9cc6456c49
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-08-22 19:06:09 -0400

    added models, updated index and route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,70 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", (req, res) => {
+ 
+  Category.findAll({
+    include: [Product],
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+   try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+    try {
+    const categoryData = await Category.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json(categoryData);
+} catch (err) {
+    res.status(500).json(err);
+};
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((results) => {
+      console.log(results);
+      if (results === 0) {
+        res.status(400).json({ message: "no changes made." });
+      } else {
+        res.json(results);
+      }
+    })
+    .catch((error) => {
+      res.json(error);
+    });
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,29 +1,87 @@
-
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((categoryData) => {
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const categoryData = await Tag.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const categoryData = await Tag.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json(categoryData);
+} catch (err) {
+    res.status(500).json(err);
+};
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((results) => {
+      console.log(results);
+      if (results === 0) {
+        res.status(400).json({ message: "no changes made." });
+      } else {
+        res.json(results);
+      }
+    })
+    .catch((error) => {
+      res.json(error);
+    });
 });
 
 module.exports = router;
